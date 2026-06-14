fcfa6a5a2ed4e217e5482cc066ca04af37940d24
Prayas <notprayasmitra@proton.me>
2026-06-14 15:48:30 +0530

    feat: Added basic content to about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,16 @@
 import "../styles/pages/about.css"
 
+import { aboutInfo } from "../data";
+
 function About() {
-  return <div>About</div>
+  return (
+    <main className="page">
+      <section className="hero">
+        <h1>About Me</h1>
+        <p className="hero-bio">{aboutInfo.description}</p>
+      </section>
+    </main>
+  )
 }
 
 export default About;
